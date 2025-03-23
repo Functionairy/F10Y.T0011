db0fdbae4d22ed4d9459d4fa4f700340cde1fe44
SURFEIT\David <david.coats@gmail.com>
2025-03-23 11:47:48 -0700

    Add marker attribute types.

---
+++ source/F10Y.T0011/Code/Attributes/IgnoreAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using F10Y.T0000;
+
+
+namespace F10Y.T0011
+{
+    /// <summary>
+    /// Allows marking any member to that it is ignored during the code survey process.
+    /// </summary>
+    [SurveyAttributeMarker]
+    [AttributeUsage(AttributeTargets.All)]
+    public class IgnoreAttribute : Attribute
+    {
+        // Do not include any is-X logic. To not ignore something, just remove the ignore element.
+    }
+}
---
+++ source/F10Y.T0011/Code/Attributes/SurveyAttributeMarkerAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using F10Y.T0000;
+
+
+namespace F10Y.T0011
+{
+    /// <summary>
+    /// Marks any code element for inclusion in a survey. (Used for marking attributes as survey attributes.)
+    /// </summary>
+    /// <remarks>
+    /// The survey attribute is useful for surveying assemblies and code, then building a catalogue of survey result code elements..
+    /// </remarks>
+    [MarkerAttributeMarker] // This marker attribute is itself a marker attribute.
+    [AttributeUsage(AttributeTargets.All, // Allow all elements to be surveyed.
+        AllowMultiple = false, // No.
+        Inherited = false // Make inheritors all use the marker type.
+    )]
+    public class SurveyAttributeMarkerAttribute : Attribute
+    {
+        /// <summary>
+        /// Allows specifying that a type is *not* a survey attribute.
+        /// This is useful for marking types that end up canonical survey attribute code file locations, but are not actually survey attributes.
+        /// </summary>
+        public bool Is_SurveyAttribute { get; }
+
+
+        public SurveyAttributeMarkerAttribute(
+            bool is_MarkerAttribute = true)
+        {
+            this.Is_SurveyAttribute = is_MarkerAttribute;
+        }
+    }
+}
