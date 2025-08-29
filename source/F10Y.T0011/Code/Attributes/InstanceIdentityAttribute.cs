770896b5e577676bf388889a5fc8b799e0ec4186
davidcoats <david@getsafetycone.com>
2025-08-28 23:11:14 -0700

    Interim commit.

--- source/F10Y.T0011/Code/Attributes/InstanceIdentityAttribute.cs
+++ source/F10Y.T0011/Code/Attributes/InstanceIdentityAttribute.cs
@@ -7,9 +7,16 @@ namespace F10Y.T0011
     /// Allows marking any member with an instance identity so that duplicates can be found.
     /// </summary>
     [SurveyAttributeMarker]
-    [AttributeUsage(AttributeTargets.All)]
+    [AttributeUsage(
+        AttributeTargets.All, // Any member can be an instance.
+        AllowMultiple = true, // Allow multiple identities.
+        Inherited = false // Nothing is inherited.
+        )]
     public class InstanceIdentityAttribute : Attribute
     {
+        /// <summary>
+        /// A unique label. (<see cref="Guid"/> values work well).
+        /// </summary>
         public string Label { get; }
 
 
